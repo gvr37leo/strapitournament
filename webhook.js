7d30432e5458285adfcaa1c7e0d928d30c0168f2
paul boon <gvr37.p.l.boon@gmail.com>
2022-03-07 20:27:55 +0100

    p

--- webhook.js
+++ webhook.js
@@ -8,30 +8,29 @@ const exec = require('child_process').exec;
 
 const PM2_CMD = 'cd /strapitournament && pm2 startOrRestart ecosystem.config.js';
 
-http
-  .createServer(function(req, res) {
-    req.on('data', function(chunk) {
-      let sig =
-        'sha1=' +
-        crypto
-          .createHmac('sha1', secret)
-          .update(chunk.toString())
-          .digest('hex');
+http.createServer(function(req, res) {
+  req.on('data', function(chunk) {
+    let sig =
+      'sha1=' +
+      crypto
+        .createHmac('sha1', secret)
+        .update(chunk.toString())
+        .digest('hex');
 
-      if (req.headers['x-hub-signature'] == sig) {
-        exec(`cd ${repo} && git pull`, (error, stdout, stderr) => {// && ${PM2_CMD}
-          if (error) {
-            console.error(`exec error: ${error}`);
-            return;
-          }
-          console.log(`stdout: ${stdout}`);
-          console.log(`stderr: ${stderr}`);
-        });
-      }
-    });
+    if (req.headers['x-hub-signature'] == sig) {
+      exec(`cd ${repo} && git pull`, (error, stdout, stderr) => {// && ${PM2_CMD}
+        if (error) {
+          console.error(`exec error: ${error}`);
+          return;
+        }
+        console.log(`stdout: ${stdout}`);
+        console.log(`stderr: ${stderr}`);
+      });
+    }
+  });
 
-    res.end();
-  })
-  .listen(8080);
- //
-  console.log('listening for pull requests on 8080') 
+  res.end();
+})
+.listen(8080);
+
+console.log('listening for pull requests on 8080') 
