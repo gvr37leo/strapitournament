d213a61ca405d2d7c4ae5f323f3bedac3cb15109
paul boon <gvr37.p.l.boon@gmail.com>
2022-03-07 20:56:20 +0100

    p

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,25 +1,25 @@
 module.exports = {
   apps: [
-    // {
-    //   name: 'strapi',
-    //   cwd: '/strapitournament/my-project',
-    //   script: 'npm',
-    //   args: 'run develop',
-    //   // env: {
-    //   //   NODE_ENV: 'production',
-    //   //   DATABASE_HOST: 'localhost', // database endpoint
-    //   //   DATABASE_PORT: '5432',
-    //   //   DATABASE_NAME: 'strapi', // DB name
-    //   //   DATABASE_USERNAME: 'your-name', // your username for psql
-    //   //   DATABASE_PASSWORD: 'password', // your password for psql
-    //   // },
-    // },
     {
-      name: 'frontend',
-      cwd: '/strapitournament/frontend',
+      name: 'strapi',
+      cwd: '/strapitournament/my-project',
       script: 'npm',
-      args: 'run start',
+      args: 'run develop',
+      // env: {
+      //   NODE_ENV: 'production',
+      //   DATABASE_HOST: 'localhost', // database endpoint
+      //   DATABASE_PORT: '5432',
+      //   DATABASE_NAME: 'strapi', // DB name
+      //   DATABASE_USERNAME: 'your-name', // your username for psql
+      //   DATABASE_PASSWORD: 'password', // your password for psql
+      // },
     },
+    // {
+    //   name: 'frontend',
+    //   cwd: '/strapitournament/frontend',
+    //   script: 'npm',
+    //   args: 'run start',
+    // },
     // {
     //   name: 'webhook',
     //   cwd: '/strapitournament',
