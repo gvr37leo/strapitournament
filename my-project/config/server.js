b80062f3bcb186c6a9763785026f6b5d0ff787e6
paul boon <gvr37.p.l.boon@gmail.com>
2022-03-14 10:06:12 +0100

    p

--- my-project/config/server.js
+++ my-project/config/server.js
@@ -2,6 +2,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  proxy: true,
   url: env('PUBLIC_URL', 'http://localhost:1337'),
   app: {
     keys: env.array("APP_KEYS", ["testKey1", "testKey2"]),
